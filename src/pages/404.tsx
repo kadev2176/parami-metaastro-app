ade773b3ebc31fee536debb2bce75764983a95b6
kadev2176 <kevinchristopher2176@gmail.com>
2022-02-21 15:47:00 +0800

    fix 404 and total display

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,10 @@
-import {config} from '@/config/config';
 import React from 'react';
 import { useEffect } from 'react';
 import { history } from 'umi';
 
 const NoFoundPage: React.FC = () => {
   useEffect(() => {
-    history.push(config.page.homePage);
+    history.push('/');
   }, []);
 
   return (
