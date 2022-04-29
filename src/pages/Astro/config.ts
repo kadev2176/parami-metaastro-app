e108d4ef2c3aca48fbb4eee172e30b055afb3af9
kadev2176 <kevinchristopher2176@gmail.com>
2022-04-29 15:30:51 +0800

    update ordinary contract address

--- src/pages/Astro/config.ts
+++ src/pages/Astro/config.ts
@@ -3,17 +3,13 @@ config();
 export const defaultChainId = 4;
 
 export const contractAddresses = {
-    ad3: {
-        1: '0x76B61Ae8F964F5CBE23ad9aea9BF597aa4cEA7eD',
-        4: '0x76B61Ae8F964F5CBE23ad9aea9BF597aa4cEA7eD',
-    },
     mint: {
         1: '0x125717E578d9B2540A129e8A710AB36623f2EDbb',
         4: '0x125717E578d9B2540A129e8A710AB36623f2EDbb',
     },
     breed: {
-        1: '0xf8dC9727F560982e2b89b188353a9498eed93F56',
-        4: '0xf8dC9727F560982e2b89b188353a9498eed93F56',
+        1: '0x0d45918e1f0d72bc2075c964C28d950E2Ff7f45c',
+        4: '0x0d45918e1f0d72bc2075c964C28d950E2Ff7f45c',
     },
 }
 
