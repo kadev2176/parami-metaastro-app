ce1250624854dedfb6cf001083be95617f955ed7
kadev2176 <kevinchristopher2176@gmail.com>
2022-07-17 04:32:04 +0800

    update: gif

--- src/pages/Index/Intro/index.tsx
+++ src/pages/Index/Intro/index.tsx
@@ -8,40 +8,30 @@ const Intro: React.FC = () => {
   return (
     <div className={style.introContainer}>
       <Row gutter={[16, 16]}>
-        <Col
-          className={style.introCol}
-          xs={24} sm={24} md={24} lg={12} xl={12}
-        >
-          <div className={style.title}>
-            What is MetaAstro?
-          </div>
+        <Col className={style.introCol} xs={24} sm={24} md={24} lg={12} xl={12}>
+          <div className={style.title}>What is MetaAstro?</div>
           <Row gutter={[16, 16]} className={style.introItem}>
             <Col span={4} className={style.introItemCol}>
-              <FaBirthdayCake
-                className={style.icon}
-              />
+              <FaBirthdayCake className={style.icon} />
             </Col>
             <Col span={20}>
-              <div className={style.subtitle}>
-                Natal chart for Metaverse
-              </div>
+              <div className={style.subtitle}>Natal chart for Metaverse</div>
               <p className={style.content}>
-                Each Primordial Gods’ uniqueness is determined by their MetaAstro, an Astrology based, hyper-personalized Natal Chart, which contains soul-bound attributes across metaverses.
+                Each Primordial Gods’ uniqueness is determined by their MetaAstro, an Astrology
+                based, hyper-personalized Natal Chart, which contains soul-bound attributes across
+                metaverses.
               </p>
             </Col>
           </Row>
           <Row gutter={[16, 16]} className={style.introItem}>
             <Col span={4} className={style.introItemCol}>
-              <MdOutlineVerified
-                className={style.icon}
-              />
+              <MdOutlineVerified className={style.icon} />
             </Col>
             <Col span={20}>
-              <div className={style.subtitle}>
-                ERC-721 based
-              </div>
+              <div className={style.subtitle}>ERC-721 based</div>
               <p className={style.content}>
-                The MetaAstro NFT (ERC 721) is generated based on your birth date, which endows your metaverse character with a series of attributes relative to yourself.
+                The MetaAstro NFT (ERC 721) is generated based on your birth date, which endows your
+                metaverse character with a series of attributes relative to yourself.
               </p>
             </Col>
           </Row>
@@ -49,46 +39,26 @@ const Intro: React.FC = () => {
         <Col xs={24} sm={24} md={24} lg={12} xl={12}>
           <Row gutter={[2, 2]} className={style.introItem}>
             <Col span={12} className={style.demoChartItem}>
-              <div className={`${style.tag} ${style.earth}`}>
-                Earth Style
-              </div>
-              <img
-                className={style.demoChart}
-                src={'/images/demo/earth.png'}
-              />
+              <div className={`${style.tag} ${style.earth}`}>Earth Style</div>
+              <img className={style.demoChart} src={'/images/demo/earth.gif'} alt="Earth" />
             </Col>
             <Col span={12} className={style.demoChartItem}>
-              <div className={`${style.tag} ${style.fire}`}>
-                Fire Style
-              </div>
-              <img
-                className={style.demoChart}
-                src={'/images/demo/fire.png'}
-              />
+              <div className={`${style.tag} ${style.fire}`}>Fire Style</div>
+              <img className={style.demoChart} src={'/images/demo/fire.png'} alt="Fire" />
             </Col>
             <Col span={12} className={style.demoChartItem}>
-              <div className={`${style.tag} ${style.water}`}>
-                Water Style
-              </div>
-              <img
-                className={style.demoChart}
-                src={'/images/demo/water.png'}
-              />
+              <div className={`${style.tag} ${style.water}`}>Water Style</div>
+              <img className={style.demoChart} src={'/images/demo/water.png'} alt="Water" />
             </Col>
             <Col span={12} className={style.demoChartItem}>
-              <div className={`${style.tag} ${style.wind}`}>
-                Air Style
-              </div>
-              <img
-                className={style.demoChart}
-                src={'/images/demo/wind.png'}
-              />
+              <div className={`${style.tag} ${style.wind}`}>Air Style</div>
+              <img className={style.demoChart} src={'/images/demo/wind.png'} alt="Wind" />
             </Col>
           </Row>
         </Col>
       </Row>
     </div>
-  )
-}
+  );
+};
 
 export default Intro;
