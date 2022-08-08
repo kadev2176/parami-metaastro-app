18b80fae0a8998ac975bd1c967cf669508471d77
kadev2176 <kevinchristopher2176@gmail.com>
2022-08-08 15:20:37 +0800

    update: deploy

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if [ $? -eq 0 ]; then
   cp index.html 404.html
   git init .
   git branch -M gh-pages
-  git remote add origin git@github.com:parami-protocol/astro-app.git
+  git remote add origin git@github.com:parami-foundation/parami-metaastro-app.git
   git add -f .
   git commit -m "Deploy"
   git push -f origin gh-pages
